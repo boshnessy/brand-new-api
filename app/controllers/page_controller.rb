cbec8b48a08faebd1ae359b58ad1c1cb70fb2120
Becca O'Shaughnessy <boshnessy@gmail.com>
2018-03-04 11:06:34 -0600

    add picture

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -1,5 +1,9 @@
 class PageController < ApplicationController
   def sunday_method
-    render html: {:message => "Happy Sunday"}
+    render html: 'Happy Sunday <img src="https://i1.wp.com/frontdoorcatering.com/wp-content/uploads/2017/11/Sunday-FUNday.jpg?fit=607%2C534">'.html_safe
+  end
+
+  def time_method
+    render json: {:time => Time.now.strftime("%r")}
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,6 @@
 Rails.application.routes.draw do
   get '/new_url' => 'about#new_method'
-end
-
-Rails.application.routes.draw do
   get '/whatever' => 'idk#whatever_method'
-end
-
-Rails.application.routes.draw do
   get '/sunday' => 'page#sunday_method'
+  get '/sunday_time' => 'page#time_method'
 end
